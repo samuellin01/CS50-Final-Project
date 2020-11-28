4d9027a7106f18a0c8fb6d07bcd464fdc1f90d06
samuellin01 <70851632+samuellin01@users.noreply.github.com>
2020-11-27 22:18:02 -0800

    Add files via upload

---
+++ application.py
@@ -0,0 +1,33 @@
+import os
+import datetime
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/")
+def index():
+    return render_template("layout.html");
+
+#layout deadline: 11/27 end of day
+
+#pages for different constellations (redirected from homepage) 11/28 (both work on it)
+
+#sign in page (M) 11/30
+
+#register (M) 11/30
+
+#form for user to input location, time, etc. (S) 11/30
+
+#
+
+#due 11/30
