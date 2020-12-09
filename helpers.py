c3f0a8157c329472b23a74733600c97651e4ee1f
Samuel Lin <samuellin@college.harvard.edu>
2020-12-08 16:46:39 -0800

    Cleanup

--- helpers.py
+++ helpers.py
@@ -81,7 +81,7 @@ def draw_constellations(ax):
                         ras.append(item[0])
                     for item in s.execute("SELECT dec FROM stars WHERE constellation = ? AND star = ?", (constellation, stars[j-i])):
                         decs.append(item[0])
-                ax.plot(numpy.radians([ras[0], ras[1]]), [-1 * decs[0]+45, -1 * decs[1] + 45], color='skyblue', linewidth=1.0)
+                ax.plot(numpy.radians([ras[0], ras[1]]), [-1 * decs[0]+45, -1 * decs[1] + 45], linewidth=1.0, color='skyblue')
                 # Add label with constellation name next to first star 
                 if i == 0:
                     ax.text(numpy.radians(ras[0]), -1 * decs[0]+45, "{}".format(constellation), fontsize=4, weight='bold')
@@ -91,7 +91,7 @@ def draw_vision(ax, input_time, input_loc):
     # Convert coordinates to altitude-azimuth coordinates and to right-ascension/declination coordinates
     azimuth = numpy.arange(0, 360.1, 1)
     altitude = numpy.zeros(len(azimuth))
-    vision = SkyCoord(azimuth, altitude, unit='deg', frame='altaz', obstime=input_time, location=input_loc).transform_to('icrs')
+    vision = SkyCoord(azimuth, altitude, frame='altaz', unit='deg', obstime=input_time, location=input_loc).transform_to('icrs')
     
     # Plot RA and DEC coordinates
     ax.plot(vision.ra.radian, -vision.dec.value+45, '-', linewidth=0.3, color='darkblue')
@@ -103,7 +103,7 @@ def draw_vision(ax, input_time, input_loc):
     curved_alt = [num for num in numpy.arange(0, 90.1, 1)]
     curved_az = len(curved_alt)*[90] + (len(curved_alt)-1)*[270]
     curved_alt = curved_alt + curved_alt[:-1][::-1]
-    curved_ax = SkyCoord(curved_az, curved_alt, unit='deg', frame='altaz', obstime=input_time, location=input_loc).transform_to('icrs')
+    curved_ax = SkyCoord(curved_az, curved_alt, frame='altaz', unit='deg', obstime=input_time, location=input_loc).transform_to('icrs')
     ax.plot(curved_ax.ra.radian, -curved_ax.dec.value+45, '-', linewidth=0.5, color='turquoise')
 
     # Draw straight axis
@@ -113,14 +113,14 @@ def draw_vision(ax, input_time, input_loc):
     # Label North, South, East, West
     directions = ['N', 'E', 'S', 'W']
     counter = 0
-    vision_circle_deg = SkyCoord(numpy.arange(0, 345.1, 15), numpy.zeros(len(numpy.arange(0, 345.1, 15))), unit='deg', frame='altaz', obstime=input_time, location=input_loc ).transform_to('icrs')
+    vision_circle_deg = SkyCoord(numpy.arange(0, 345.1, 15), numpy.zeros(len(numpy.arange(0, 345.1, 15))), frame='altaz', unit='deg', obstime=input_time, location=input_loc).transform_to('icrs')
     for (coordinate, label) in zip(vision_circle_deg, numpy.arange(0, 345.1, 15)):
         if int(label) % 90 == 0:
-            ax.text(coordinate.ra.radian, -coordinate.dec.value+45, directions[counter], fontsize=10, fontname="serif", fontweight='bold' )
+            ax.text(coordinate.ra.radian, -coordinate.dec.value+45, directions[counter], fontname="serif", fontsize=10, fontweight='bold' )
             counter += 1
 
 # Draw location of moon at input time
 def draw_moon(ax, input_time, input_loc):
     moon = get_moon(input_time, location=input_loc)
     moon = SkyCoord(moon.ra, moon.dec, frame='gcrs').transform_to('icrs')
-    ax.plot([moon.ra.radian], [-moon.dec.value+45], linestyle='', color='yellow', marker='$🌛$')
+    ax.plot([moon.ra.radian], [-moon.dec.value+45], color='yellow', linestyle='', marker='$🌛$')
